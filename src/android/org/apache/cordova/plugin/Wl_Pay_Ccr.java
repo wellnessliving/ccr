41753816a555e66964f193909979039ade9de053
Gromakov <gromakov94@gmail.com>
2018-08-03 18:37:48 +0300

    wl-14468: [Spa-Staff] Volume should be set to Max when inserting a card swiper.

--- src/android/org/apache/cordova/plugin/Wl_Pay_Ccr.java
+++ src/android/org/apache/cordova/plugin/Wl_Pay_Ccr.java
@@ -223,9 +223,10 @@ public class Wl_Pay_Ccr extends CordovaPlugin
       return;
     }
 
-    AudioManager audioManager = (AudioManager)getSystemService("audio");
+    String audio = Context.AUDIO_SERVICE;
+    /*AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 
-    /*int max = manager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
+    int max = manager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
     manager.setStreamVolume(AudioManager.STREAM_ALARM,max,AudioManager.FLAG_SHOW_UI);
 
     int max = manager.getStreamMaxVolume(AudioManager.STREAM_DTMF);
